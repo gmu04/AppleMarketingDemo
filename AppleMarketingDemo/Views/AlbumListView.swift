8be45365b45f3c2c899b5d4666b4be1cd5b65ad3
GokhanMutlu <gokhanmutlu04@yahoo.com>
2022-03-16 21:06:59 +0300

    ** Add Search Module base view, and "Pull To Refresh" feature

--- AppleMarketingDemo/Views/AlbumListView.swift
+++ AppleMarketingDemo/Views/AlbumListView.swift
@@ -6,6 +6,7 @@ import Combine
 struct AlbumListView: View {
 	
 	@ObservedObject var albumListVM = AlbumListViewModel(session: URLSession.shared)
+	@State private var showModal = false
 	
     var body: some View {
 		NavigationView {
@@ -21,8 +22,26 @@ struct AlbumListView: View {
 					}
 				}
 				.listStyle(.plain)
+				.refreshable {
+					albumListVM.getLaunchList()
+				}
+			}
+			.sheet(isPresented: $showModal) {
+				
+			} content: {
+				SearchModuleView()
 			}
 			.navigationBarTitle("Albums", displayMode: .inline)
+			.navigationBarItems(
+				trailing:
+					Button(action: {
+						self.showModal.toggle()
+					}, label: {
+						Image(systemName:"magnifyingglass")
+							.resizable()
+							.frame(width: 24, height: 24)
+					})
+			)
 		}
     }
 }
---
+++ AppleMarketingDemo/Views/SearchModuleView.swift
@@ -0,0 +1,16 @@
+// SearchModuleView.swift by Gokhan Mutlu on 16.03.2022
+
+import SwiftUI
+
+struct SearchModuleView: View {
+    var body: some View {
+        Text("Search Module")
+			.font(.largeTitle)
+    }
+}
+
+struct SearchModuleView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchModuleView()
+    }
+}
