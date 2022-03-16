9d9d258d8a360f344deefe16d197764dc28de2ea
GokhanMutlu <gokhanmutlu04@yahoo.com>
2022-03-16 09:08:57 +0300

    Initial Commit

---
+++ AppleMarketingDemo/AppleMarketingDemoApp.swift
@@ -0,0 +1,12 @@
+// AppleMarketingDemoApp.swift by Gokhan Mutlu on 16.03.2022
+
+import SwiftUI
+
+@main
+struct AppleMarketingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
